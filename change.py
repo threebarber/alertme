adc47639638f4bd6c660e0d08bdd90cdda5f576b
threebarber <jblack25@students.towson.edu>
2017-06-06 01:29:47 -0400

    Add files via upload

--- change.py
+++ change.py
@@ -2,16 +2,17 @@
 import time
 import requests
 import smtplib
-from timeit import default_timer as timer
 from config import *
+import tweepy
+from datetime import datetime
 
-
-def send_email(user, pwd, recipient, subject, body): #snippet courtesy of david / email sending function
+def send_email(user, pwd, recipient): #snippet courtesy of david / email sending function
+    SUBJECT             = 'SITE UPDATED' #message subject
+    body                = 'CHANGE AT ' + str(url) #message body
     gmail_user = user
     gmail_pwd = pwd
     FROM = user
     TO = recipient if type(recipient) is list else [recipient]
-    SUBJECT = subject
     TEXT = body
 
     # Prepare actual message
@@ -28,52 +29,46 @@ def send_email(user, pwd, recipient, subject, body): #snippet courtesy of david 
     except Exception, e: #else tell user it failed and why (exception e)
         print "[-]Failed to send notification email, " +str(e)
 
+def sendtweet(consumer_key, consumer_secret,access_token, access_token_secret,status_string):
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        api.update_status(status_string)
+    except tweepy.error.TweepError:
+        print "[-]Error, invalid or expired twitter tokens, visit http://apps.twitter.com to retrieve or refresh them"
+
 def main():
-    print "[+]Starting up monitor on " +url+ ",notify on change detect is set to " +str(notify)
-    start = timer()
-    
-    with requests.Session() as c:
-        try:
-            page1 = c.get(url) #base page that will be compared against
-        except Exception, e:
-            print "[-]Error Encountered during initial page retrieval: " +e
-         
-        while 1:
-                time.sleep(wait_time) #wait beetween comparisons
-        
-                try:
-                    page2 = c.get(url) #page to be compared against page1 / the base page
-                except Exception, e:
-                    print "[+]Error Encountered during secondary page retrieval: " +e
-        
-                if page1.content == page2.content: #if else statement to check if content of page remained same
-                    end = timer()
-                    if ((end-start)) >= 60:
-                        timeMinutes = (end-start) / 60
-                        print "[-]No Change Detected @ " +str(url)+ "\n[-]Elapsed Time: " +str(timeMinutes)+ " minutes"
+        print "[+]Starting up monitor on " +url+ ",notify on change detect is set to " +str(notify)
+
+        with requests.Session() as c:
+            try:
+                page1 = c.get(url) #base page that will be compared against
+            except Exception, e:
+                print "[-]Error Encountered during initial page retrieval: " +e
+             
+            while 1:
+                    time.sleep(wait_time) #wait beetween comparisons
+                    try:
+                        page2 = c.get(url, headers={'User-Agent':"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"}) #page to be compared against page1 / the base page
+                    except Exception, e:
+                        print "[+]Error Encountered during comparison page retrieval: " +e
+            
+                    if page1.content == page2.content: #if else statement to check if content of page remained same
+                        print '[-]No Change Detected on ' +str(url)+ "\n" +str(datetime.now())
                     else:
-                        print '[-]No Change Detected @ ' +str(url)+ "\n[+]Elapsed Time: " +str((end-start))+ " seconds"
-                    
-                else:
-                    end = timer()
-                    if int((end-start)) >= 60:
-                        timeMinutes = (end-start) / 60
-                        print '[+]Change Detected  \n[+]Elapsed Time: ' +str(timeMinutes)+ " minutes" #if anything was changed - it sends an email alerting the user
+                        status_string = 'Change Detected at ' +str(url)+ "\n" +str(datetime.now())
+                        print "[+]"+status_string
                         if notify == True:
-                            send_email(user, pwd, recipient, subject, body) #send notification email
+                            send_email(user, pwd, recipient) #send notification email
                         else:
                             pass
-                        print '\n[+]Retrieving new base page and restarting\n' 
-                        main()
-                    else:
-                        print '[+]Change Detected  \n[+]Elapsed Time: ' +str((end-start))+ " seconds"  #if anything was changed - it sends an email alerting the user
-                        if notify == True:
-                            send_email(user, pwd, recipient, subject, body) #send notification email
+                        if tweet == True:
+                            sendtweet(consumer_key, consumer_secret,access_token, access_token_secret,status_string)
                         else:
                             pass
-                        print '\n[+]Retrieving new base page and restarting\n'     
+                        print '\n[+]Retrieving new base page and restarting\n'
                         main()
-                    
 if __name__ == '__main__':
     main()
 
